327cff13b70602357a76348db6187cf724f064e8
charanhu1 <charanhumail@gmail.com>
2022-06-02 12:37:46 +0530

    Binary serach

---
+++ Code.cpp
@@ -0,0 +1,39 @@
+//Binary search program'
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+
+int main(){
+    int low, high, mid, num, count = 0;
+    cout << "Enter the number of elements in the array: ";
+    cin >> num;
+    int arr[num];
+    cout << "Enter the elements of the array: ";
+    for(int i = 0; i < num; i++){
+        cin >> arr[i];
+    }
+    cout << "Enter the number to be searched: ";
+    cin >> num;
+    low = 0;
+    high = num - 1;
+    while(low <= high){
+        mid = (low + high) / 2;
+        if(arr[mid] == num){
+            cout << "The number is found at index " << mid << endl;
+            break;
+        }
+        else if(arr[mid] < num){
+            low = mid + 1;
+        }
+        else{
+            high = mid - 1;
+        }
+        count++;
+    }
+    if(low > high){
+        cout << "The number is not found in the array" << endl;
+    }
+    cout << "The number of comparisons is " << count << endl;
+    return 0;
+    
+}
